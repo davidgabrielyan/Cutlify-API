e23876d5c75a8fa37885a83b65be28f969bedbc5
davidgabrielyan <gabrielyandavo144707@gmail.com>
2022-05-13 18:14:32 +0400

    Fix: Add default error code

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,6 +2,7 @@ export default (err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
-  res.status(err.code);
+  const code = err.code || 500;
+  res.status(code);
   return res.json({ message: err.message });
 };
